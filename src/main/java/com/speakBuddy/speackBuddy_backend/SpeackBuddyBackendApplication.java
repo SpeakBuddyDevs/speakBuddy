7eeee5c39797abec8ba3eb5b6dc54c621e6b5c4f
pduran24 <pabloduranp5@gmail.com>
2025-11-04 20:08:04 +0100

    Primer commit y creacion de la base de datos

---
+++ src/main/java/com/speakBuddy/speackBuddy_backend/SpeackBuddyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.speakBuddy.speackBuddy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeackBuddyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeackBuddyBackendApplication.class, args);
+	}
+
+}
