105b76a96db3cc23d6734edce34bc80c8a650ed8
unknown <juancastillon24092001@gmail.com>
2025-11-07 16:36:27 +0100

    Creacion de metodo LogOut y terminar el LogIn

--- src/main/java/com/speakBuddy/speackBuddy_backend/service/AuthService.java
+++ src/main/java/com/speakBuddy/speackBuddy_backend/service/AuthService.java
@@ -2,20 +2,43 @@ package com.speakBuddy.speackBuddy_backend.service;
 
 import com.speakBuddy.speackBuddy_backend.dao.UserDAO;
 import com.speakBuddy.speackBuddy_backend.models.User;
+import lombok.Getter;
 import org.springframework.security.crypto.bcrypt.BCrypt;
+
+import java.util.logging.Logger;
 
 public class AuthService {
 
     private UserDAO userDAO = new UserDAO();
 
+    Logger log = Logger.getLogger(AuthService.class.getName());
+
+    @Getter
+    private User userLogged;
+
     private boolean validateUser(String email, String password) {
         User user = userDAO.findByEmail(email);
 
-        if (user == null) {
-            return false; //El usuario no existe
+        if (user != null && BCrypt.checkpw(password, user.getPassword())) {
+            userLogged = user;
+            log.info("User logged successfully");
+            return true; //El usuario no existe
         }
-
-        return BCrypt.checkpw(password, user.getPassword());
+        else {
+            log.info("User not found");
+            return false;
+        }
     }
+
+    private void logout(){
+        if (userLogged != null){
+            log.info("Logout from user: " + userLogged.getEmail());
+            userLogged = null;
+        }else {
+            log.info("There is no user logged" );
+        }
+    }
+
+
 
 }
