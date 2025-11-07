add8c4fb53ddd8ca5ec1ce506d5bf672822a5626
pduran24 <pabloduranp5@gmail.com>
2025-11-07 13:49:58 +0100

    id como Long

--- src/main/java/com/speakBuddy/speackBuddy_backend/models/Language.java
+++ src/main/java/com/speakBuddy/speackBuddy_backend/models/Language.java
@@ -13,7 +13,7 @@ public class Language {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
     private String name;
     private String isoCode; // es, en, fr, etc. -> iconos de banderas en flutter
 }
