7ab9c0c3691899d093a6a931ba92ebae87f40ebb
pduran24 <pabloduranp5@gmail.com>
2025-11-07 16:06:20 +0100

    Terminada entidad Language

--- src/main/java/com/speakBuddy/speackBuddy_backend/models/Language.java
+++ src/main/java/com/speakBuddy/speackBuddy_backend/models/Language.java
@@ -15,7 +15,9 @@ public class Language {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
+    @Column(nullable = false, unique = true, length = 100)
     private String name;
+
+    @Column(nullable = false, unique = true, length = 10)
     private String isoCode; // es, en, fr, etc. -> iconos de banderas en flutter
 }
