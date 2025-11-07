9ab6047a72acde4250f45054410076f9147a08ba
pduran24 <pabloduranp5@gmail.com>
2025-11-07 19:02:42 +0100

    Añado SecurityFilterChain para el protocolo de seguridad Spring en SecurityConfig

--- src/main/java/com/speakBuddy/speackBuddy_backend/security/SecurityConfig.java
+++ src/main/java/com/speakBuddy/speackBuddy_backend/security/SecurityConfig.java
@@ -2,14 +2,32 @@ package com.speakBuddy.speackBuddy_backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration // Le dice a Spring que esta clase contiene configuraciones de seguridad (define Beans)
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean // Crea un Bean para el PasswordEncoder que utiliza BCrypt
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .authorizeHttpRequests(authz -> authz
+
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
     }
 }
