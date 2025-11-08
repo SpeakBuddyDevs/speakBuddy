23e90b6c52011df844443fb74a965b524b86710e
pduran24 <pabloduranp5@gmail.com>
2025-11-08 17:30:04 +0100

    Implementacion DTO de peticion ProfileUpdateDTO

---
+++ src/main/java/com/speakBuddy/speackBuddy_backend/dto/ProfileUpdateDTO.java
@@ -0,0 +1,14 @@
+package com.speakBuddy.speackBuddy_backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProfileUpdateDTO {
+
+    // Campos que el usuario PUEDE cambiar
+    private String name;
+    private String surname;
+    private String profilePictureUrl;
+}
