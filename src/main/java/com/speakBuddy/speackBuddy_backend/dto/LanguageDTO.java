59b1c0b05938a51efabbe05f8016ec89ee9dd038
pduran24 <pabloduranp5@gmail.com>
2025-11-08 17:23:37 +0100

    Implementacion POJO LanguageDTO

---
+++ src/main/java/com/speakBuddy/speackBuddy_backend/dto/LanguageDTO.java
@@ -0,0 +1,14 @@
+package com.speakBuddy.speackBuddy_backend.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LanguageDTO {
+
+    private Long id;
+    private String name;
+    private String isoCOde;
+}
--- src/main/java/com/speakBuddy/speackBuddy_backend/dto/ProfileResponseDTO.java
+++ src/main/java/com/speakBuddy/speackBuddy_backend/dto/ProfileResponseDTO.java
@@ -6,7 +6,19 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ProfileResponseDTO {
+
+    // Campos de User que se quieren exponer en el perfil
     private Long id;
+    private String email;
+    private String username;
     private String name;
-    private String isoCode;
+    private String surname;
+    private String profilePictureURL;
+
+    // Campos de gamificacion
+    private Integer level;
+    private Long experiencePoints;
+
+
+
 }
