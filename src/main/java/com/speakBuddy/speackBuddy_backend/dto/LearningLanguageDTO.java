b02c94af9a592c76e54a0cb98f91636b7469d21c
pduran24 <pabloduranp5@gmail.com>
2025-11-08 17:25:14 +0100

    Implementacion POJO LearningLanguageDTO

---
+++ src/main/java/com/speakBuddy/speackBuddy_backend/dto/LearningLanguageDTO.java
@@ -0,0 +1,12 @@
+package com.speakBuddy.speackBuddy_backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LearningLanguageDTO {
+
+    private LanguageDTO language;
+    private String levelName;1
+}
