5f7ee9178816abae58e3ba4fbf2767843fd928d9
pduran24 <pabloduranp5@gmail.com>
2025-11-08 17:18:25 +0100

    Implementacion POJO ProfileResponseDTO

---
+++ src/main/java/com/speakBuddy/speackBuddy_backend/dto/ProfileResponseDTO.java
@@ -0,0 +1,12 @@
+package com.speakBuddy.speackBuddy_backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProfileResponseDTO {
+    private Long id;
+    private String name;
+    private String isoCode;
+}
