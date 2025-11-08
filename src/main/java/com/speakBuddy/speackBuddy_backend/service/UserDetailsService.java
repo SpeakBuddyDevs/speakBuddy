be3ba67df9967797dc230c7b0daa64a1e902c25a
juancastillon24 <juancastillon24092001@gmail.com>
2025-11-08 18:10:21 +0100

    Modificacion pom.xml, añadir metodo auteauthenticationManager a SecurityConfig y creacion de CustomUserDetailsService y UserDetailsService

---
+++ src/main/java/com/speakBuddy/speackBuddy_backend/service/UserDetailsService.java
@@ -0,0 +1,9 @@
+package com.speakBuddy.speackBuddy_backend.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface UserDetailsService {
+
+    UserDetails loadUserByUsername(String email) throws UsernameNotFoundException;
+}
