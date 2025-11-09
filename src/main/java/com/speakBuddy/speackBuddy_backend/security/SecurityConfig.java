f590013278e793a70e4fc6006f5bde82bc12213c
juancastillon24 <juancastillon24092001@gmail.com>
2025-11-09 20:32:50 +0100

    Modificar SecurityConfig

--- src/main/java/com/speakBuddy/speackBuddy_backend/security/SecurityConfig.java
+++ src/main/java/com/speakBuddy/speackBuddy_backend/security/SecurityConfig.java
@@ -1,18 +1,27 @@
 package com.speakBuddy.speackBuddy_backend.security;
 
+import com.speakBuddy.speackBuddy_backend.service.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration // Le dice a Spring que esta clase contiene configuraciones de seguridad (define Beans)
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
 
     @Bean // Crea un Bean para el PasswordEncoder que utiliza BCrypt
     public PasswordEncoder passwordEncoder() {
@@ -29,11 +38,17 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
 
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
                 .authorizeHttpRequests(authz -> authz
 
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 );
+
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
