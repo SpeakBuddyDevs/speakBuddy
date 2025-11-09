01f616905dbce701816297f835dff139705892ab
pduran24 <pabloduranp5@gmail.com>
2025-11-09 19:16:35 +0100

    Aclaración del dto LearningLanguageDTO

--- src/main/java/com/speakBuddy/speackBuddy_backend/dto/LearningLanguageDTO.java
+++ src/main/java/com/speakBuddy/speackBuddy_backend/dto/LearningLanguageDTO.java
@@ -3,6 +3,7 @@ package com.speakBuddy.speackBuddy_backend.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+// Para mostrar el idioma que el usuario está aprendiendo junto con su nivel (no sirve para crear)
 @Getter
 @Setter
 public class LearningLanguageDTO {
